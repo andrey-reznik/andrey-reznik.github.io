a2faadb68eec4716a0428c16803a57bf25fab281
Andrey Reznik <andrey.reznik@yahoo.com>
2024-03-02 10:16:26 +0300

    chore: update ritmy

--- _nuxt/teachers_page_bg.F3szGoAI.js
+++
@@ -1,1 +0,0 @@
-import"./entry.s_UWWT6a.js";const p=""+new URL("teachers_page_bg.xfmueLZj.png",import.meta.url).href;export{p};
---
+++ assets/teachers_page_bg.k_HFcmIh.js
@@ -0,0 +1,1 @@
+import"./entry.3WtAOoPb.js";const p=""+new URL("teachers_page_bg.xfmueLZj.png",import.meta.url).href;export{p};
